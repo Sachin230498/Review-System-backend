962572d6423eaab415529461177a3c87d911a96c
Sachin230498 <sachinpathe230498@gmail.com>
2024-03-01 01:45:53 +0530

    s

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/review");
+    await mongoose.connect("mongodb://0.0.0.0:27017/review");
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error);
