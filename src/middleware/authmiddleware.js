59ae8002d524309dcd8a14f21104d567a04c473e
Sachin230498 <sachinpathe230498@gmail.com>
2024-02-29 18:55:40 +0530

    add

--- src/middleware/authmiddleware.js
+++ src/middleware/authmiddleware.js
@@ -1,22 +1,74 @@
+// // middleware/authMiddleware.js
+// const jwt = require("jsonwebtoken");
+// const { secret } = require("../config/auth");
+
+// const authMiddleware = (req, res, next) => {
+//   const token = req.headers.authorization;
+
+//   if (!token) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, secret);
+//     req.user = decoded;
+//     next();
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(403).json({ message: "Invalid token" });
+//   }
+// };
+
+// module.exports = authMiddleware;
+
+// // middleware/authMiddleware.js
+// const jwt = require("jsonwebtoken");
+// const { secret } = require("../config/auth");
+
+// const authMiddleware = (req, res, next) => {
+//   const token = req.headers.authorization;
+
+//   if (!token) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, secret);
+//     req.user = decoded;
+//     next();
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(403).json({ message: "Invalid token" });
+//   }
+// };
+
+// module.exports = authMiddleware;
+
+
 // middleware/authMiddleware.js
-const jwt = require("jsonwebtoken");
-const { secret } = require("../config/auth");
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config/auth');
 
 const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: 'Unauthorized' });
   }
 
   try {
     const decoded = jwt.verify(token, secret);
     req.user = decoded;
+    if (req.user.role !== 'Admin') {
+      return res.status(403).json({ message: 'Forbidden: Only admin can perform this action' });
+    }
     next();
   } catch (error) {
     console.error(error);
-    return res.status(403).json({ message: "Invalid token" });
+    return res.status(403).json({ message: 'Invalid token' });
   }
 };
 
 module.exports = authMiddleware;
+
+
